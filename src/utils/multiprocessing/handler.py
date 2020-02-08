4584dc3034e6c77e881e960a3dcf8e248d2ff159
Kamuish <andremiguel952@gmail.com>
2020-02-08 19:08:36 +0000

    multiprocessing for the fitness function; to be tested

--- src/utils/multiprocessing/handler.py
+++ src/utils/multiprocessing/handler.py
@@ -1,4 +1,29 @@
 
+from multiprocessing import Queue
+import numpy as np 
+from multiprocessing import Process 
+from .worker import worker
+
 
 def multiproc_handler(population, **kwargs):
-    pass
+    
+    blocks = np.array_split(population, kwargs['processes'])
+    number_blocks = len(blocks)
+    tasks_queue  = Queue() 
+    results_queue = Queue()
+
+    [tasks_queue.put(i) for i in blocks]  # populate the task queue with all of the blocks
+
+    for k in range(kwargs['processes']):
+        process = Process(target=worker, args = (tasks_queue, results_queue), kwargs=**kwargs)
+        process.start()
+        
+    number_responses = 0
+    
+    outputs_complete = {}
+    while number_responses != number_blocks:
+        output = results_queue.get() 
+        outputs_complete = {**outputs_complete, **output}
+
+    return outputs_complete
+multiproc_handler([1,2,3,4,5],** {'processes':3})
--- src/utils/multiprocessing/worker.py
+++ src/utils/multiprocessing/worker.py
@@ -1,7 +1,7 @@
 
+from multiprocessing import Queue
 
-
-def worker(population, **kwargs):
+def worker(in_queue, out_queue, **kwargs):
     """ 
     Parameters
     ------------
@@ -10,6 +10,7 @@ def worker(population, **kwargs):
         Dictionary -> keys are the IDs and values the parameters
 
     """
+
     try:
         initial_configuration = kwargs['initial_setup']
         configuration_init = initial_configuration(**kwargs)
@@ -19,8 +20,22 @@ def worker(population, **kwargs):
     fitness_function = kwargs['fit_func']
 
     calculated_fitness = {}
-    for ID, parameters in population.items():
-        fit_level = fitness_function(parameters = parameters,
-                                    initial_config = configuration_init,
-                                    **initial_configuration)
-        calculated_fitness[ID] = fit_level
+
+    while True:
+        try:
+            population = in_queue.get(timeout=1)
+        except Queue.Empty:
+            return
+        except Exception as e:
+            print("Oh, another error: ", e)
+            return 
+
+        for individual in population:
+            ID = individual.ID
+            parameters = individual.parameters
+            fit_level = fitness_function(parameters = parameters,
+                                        initial_config = configuration_init,
+                                        **initial_configuration)
+            calculated_fitness[ID] = fit_level
+            
+        out_queue.put(calculated_fitness)
