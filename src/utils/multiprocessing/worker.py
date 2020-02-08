a2c7861030a60c097ed374d89ee7dea1214af95b
Kamuish <andremiguel952@gmail.com>
2020-02-08 18:28:19 +0000

    general form of the worker; missing comms

--- src/utils/multiprocessing/worker.py
+++ src/utils/multiprocessing/worker.py
@@ -2,4 +2,25 @@
 
 
 def worker(population, **kwargs):
-    pass 
+    """ 
+    Parameters
+    ------------
+    
+    population: dict
+        Dictionary -> keys are the IDs and values the parameters
+
+    """
+    try:
+        initial_configuration = kwargs['initial_setup']
+        configuration_init = initial_configuration(**kwargs)
+    except KeyError as e:
+        initial_configuration = None 
+
+    fitness_function = kwargs['fit_func']
+
+    calculated_fitness = {}
+    for ID, parameters in population.items():
+        fit_level = fitness_function(parameters = parameters,
+                                    initial_config = configuration_init,
+                                    **initial_configuration)
+        calculated_fitness[ID] = fit_level
