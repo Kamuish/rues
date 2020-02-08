1c797a3e2750ed9b00bea7b3efb4f7c0facb0407
Kamuish <andremiguel952@gmail.com>
2020-02-08 17:23:31 +0000

    random pairing off the parents

--- src/selection_algorithms/roulette_wheel.py
+++ src/selection_algorithms/roulette_wheel.py
@@ -1,6 +1,7 @@
 
 import numpy as  np 
-def roulette_wheel(population, offspring_number):
+from .create_random_pairs import create_rand_pairs
+def roulette_wheel(population, offspring_number, **kwargs):
     """
     Roulette wheel selection
 
@@ -8,21 +9,22 @@ def roulette_wheel(population, offspring_number):
 
     fitness_sum = np.sum([ind.score for ind in population])
     chosen_elemts = []
-    for k in range(offspring_number):
-        for numb in range(2): # to choose the two parents
-            random_number = np.random.uniform(0,fitness_sum, size=1)[0]
-            partial_sum = 0
-            found_two_parents = False 
-            for individ in population:
-                partial_sum += individ.score  
-                if random_number < partial_sum:
-                    individ.parent = True
-                    if numb == 0:
-                        chosen_elemts.append([individ.ID])
-                    else:
-                        chosen_elemts[-1].append(individ.ID)
-                        found_two_parents = True
-                    break
-    return chosen_elemts
+    for k in range(2*offspring_number):
+        random_number = np.random.uniform(0,fitness_sum, size=1)[0]
+        partial_sum = 0
+        found_two_parents = False 
+
+        for individ in population:
+            partial_sum += individ.score  
+            if random_number < partial_sum:
+                individ.parent = True
+                if numb == 0:
+                    chosen_elemts.append(individ)
+                else:
+                    chosen_elemts.append(individ.ID)
+                    found_two_parents = True
+                break
+            
+    return create_rand_pairs(chosen_elements)
 
 
