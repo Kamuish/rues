380f15c461593bbcf72933215d118cd1a6fe4bbe
Kamuish <andremiguel952@gmail.com>
2020-02-09 18:23:00 +0000

    retrieving optimal parameters

--- src/genetic_algo.py
+++ src/genetic_algo.py
@@ -17,7 +17,7 @@ class Genetic():
         self._population = Population(pop_size, param_limits) 
         self._completed_fit = False
         self._config_dict = config_dict
-        
+
     def fit(self, X, Y):
         """
         Run the genetic algorithm to find the best parameters for the orbit
@@ -33,11 +33,20 @@ class Genetic():
             Taken and the median of the entire final population +84th percentile -16th percentile
 
             I.e. build a 68% confidence interval around the fitted value
+
+        Returns
+        --------
+            Dictionary with sets inside (median value, median - 16th percentile, 84th percentile - median)
         """
         if not self._completed_fit:
             raise Exception("Genetic algorithm fit is yet to be used;")
-        pass 
+        
+        param_dict = self._population.get_population_parameters()
 
-        percentiles = np.sqrt(np.nanpercentile(vararr, [16, 50, 84])) / np.sqrt(norm)
+        output = {}
 
+        for parameter, values in param_dict.items():
+            percentiles = np.sqrt(np.nanpercentile(vararr, [16, 50, 84])) / np.sqrt(norm)
+            output[parameter] =  (percentiles[1], percentiles[1] - percentiles[0], percentiles[2] - percentiles[1] )
 
+        return output
