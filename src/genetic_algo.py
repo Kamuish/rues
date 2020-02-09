33ccaafeaaca40cf0143b9d31c0841e55c6eb513
Kamuish <andremiguel952@gmail.com>
2020-02-09 18:18:10 +0000

    initializaing the population

--- src/genetic_algo.py
+++ src/genetic_algo.py
@@ -1,8 +1,8 @@
-
-
+import numpy as np 
+from .Population import Population
 
 class Genetic():
-    def __init__(self, fitness_func, pop_size, param_values):
+    def __init__(self, pop_size, param_limits, config_dict):
         """
 
         Parameters
@@ -14,15 +14,17 @@ class Genetic():
         param_limits: dict
             Dictionary with the upper and lower value of the model parameters
         """
-        pass 
+        self._population = Population(pop_size, param_limits) 
         self._completed_fit = False
-
+        self._config_dict = config_dict
+        
     def fit(self, X, Y):
         """
         Run the genetic algorithm to find the best parameters for the orbit
         """
         pass
         self._completed_fit = True 
+
 
     def get_optimal_params(self):
         """
@@ -35,3 +37,7 @@ class Genetic():
         if not self._completed_fit:
             raise Exception("Genetic algorithm fit is yet to be used;")
         pass 
+
+        percentiles = np.sqrt(np.nanpercentile(vararr, [16, 50, 84])) / np.sqrt(norm)
+
+
