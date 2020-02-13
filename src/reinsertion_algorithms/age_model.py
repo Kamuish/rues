7ef246413a4da69d7f42656ecd833b52dfce4012
Kamuish <andremiguel952@gmail.com>
2020-02-13 09:58:28 +0000

    reinsertion based on age

---
+++ src/reinsertion_algorithms/age_model.py
@@ -0,0 +1,20 @@
+
+  
+# https://www.sheffield.ac.uk/polopoly_fs/1.60188!/file/manual.pdf
+
+# page 21
+
+from src.utils import sorted_population
+
+def age_based_selection(population, to_keep):
+    """
+    Select only the younger individuals to contine to the next generation
+
+    the n_keep new individuals will substitute the "old" elements
+
+    """
+
+
+    sort_pop = sorted_population(population, 'creation_age')   # the older elements will be at the end of the list 
+
+    return sort_pop[0:to_keep]
