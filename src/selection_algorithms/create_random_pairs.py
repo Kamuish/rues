349e11863c90c44738b9e72f3389fea781d9d1b0
Kamuish <andremiguel952@gmail.com>
2020-02-13 18:36:19 +0000

    was overcounting offspring numb by 2

--- src/selection_algorithms/create_random_pairs.py
+++ src/selection_algorithms/create_random_pairs.py
@@ -7,6 +7,6 @@ def pop_random(lst):
 def create_rand_pairs(chosen_elements):
     # create random pairs out of the chosen elements
     chosen_pairs = []
-    for k in range(offspring_number): # list will always have an even number of elements
+    for k in range(int(len(chosen_elements)/2)): # list will always have an even number of elements
         chosen_pairs.append([pop_random(chosen_elements), pop_random(chosen_elements)])
     return chosen_pairs
