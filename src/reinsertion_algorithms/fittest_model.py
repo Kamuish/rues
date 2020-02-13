e4d4140e9608f765cac145a8d013e1e5e5baa845
Kamuish <andremiguel952@gmail.com>
2020-02-13 09:58:16 +0000

    now only staying with fittest individuals

---
+++ src/reinsertion_algorithms/fittest_model.py
@@ -0,0 +1,8 @@
+
+from src.utils import sorted_population
+
+
+def fittest_individuals(population, to_keep):
+    
+    sorted_pop =  sorted_population(population, 'score') # fittest individuals at the end
+    return sorted_pop[-to_keep:]
